844c75ff4784440a58a2ca3f12044807cb1928fa
HeakMeng <phengmengheak@outlook.com>
2025-12-23 23:44:50 +0700

    Added cluster for running in worker and master

--- node-performance/server.js
+++ node-performance/server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const cluster = require('node:cluster');
+
+
+cluster.schedulingPolicy = cluster.SCHED_RR;
 
 const app = express();
 
@@ -7,15 +11,23 @@ function delay(duration){
   while(Date.now() - startTime < duration){
     //event loop is block
   }
-}
+};
 
 app.get('/', (req, res) => {
-  res.send('Hello dear! this is my home');
-})
+  res.send(`Performance example ${process.pid}`);
+});
 
 app.get('/timer', (req, res) => {
   delay(9000);
-  res.send('Halo Dexte, ding ding ding!');
-})
+  res.send(`Performance example ${process.pid}`);
+});
 
-app.listen(3000);
+console.log("Server started is...");
+if(cluster.isPrimary){
+  console.log("Master Node has been started");
+  cluster.fork(); // -> create a worker node
+  cluster.fork(); // -> If this scenarios we will create 2 worker(called 2 times)
+} else{
+  console.log("Worker Node has been started");
+  app.listen(3000);
+}
