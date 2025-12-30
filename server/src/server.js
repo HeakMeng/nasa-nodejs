679be0ab50649bb8f9d822c0088c792dcef1aeb0
HeakMeng <phengmengheak@outlook.com>
2025-12-30 09:31:00 +0700

    Added launch schema

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,21 @@
 const http = require('http');
 const app = require('./app');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8000; 
 
+const MongoURL = 'mongodb+srv://heakmeng:heak1122@nasa.kzcvdnq.mongodb.net/?appName=nasa';
+
 const server = http.createServer(app);
+
+mongoose.connection.once('open',() => {
+  console.log('Mongodb connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+});
+
 const { loadPlanets } = require("./models/planets.model");
 
 server.listen(PORT, () => {
@@ -11,7 +23,9 @@ server.listen(PORT, () => {
 });
 
 async function startServer(){
+  await mongoose.connect(MongoURL); 
   await loadPlanets();
+
   app.listen(PORT, (req, res) => {
     console.log(`Server is listening on port ${PORT}`);
   });
